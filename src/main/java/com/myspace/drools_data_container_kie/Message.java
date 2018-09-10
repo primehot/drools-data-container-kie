ce41414e2ec55d1167a504cb1ee3862cf2370b61
Oleh Tsyupa <oleh.tsyupa@vubiquity.com>
2018-09-10 15:03:14 +0200

    Init

---
+++ src/main/java/com/myspace/drools_data_container_kie/Message.java
@@ -0,0 +1,35 @@
+package com.myspace.drools_data_container_kie;
+
+/**
+ * created by OTsyupa
+ */
+public class Message {
+
+    public static final int HELLO = 0;
+    public static final int GOODBYE = 1;
+
+    private String message;
+
+    private int status;
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return " NEWToString()";
+    }
+}
